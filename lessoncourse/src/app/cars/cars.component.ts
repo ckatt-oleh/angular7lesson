aa91ff0fbfd192f751e10c7044260a19f232fc49
Hordiienko Oleh <hordienko.oleh@gmail.com>
2019-05-27 17:41:40 +0300

    if, for angular

---
+++ lessoncourse/src/app/cars/cars.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: '[app-cars]',
+  selector: 'app-cars',
+  templateUrl: './cars.component.html',
+  styleUrls: ['./cars.component.css']
+})
+export class CarsComponent {
+
+  carName = '';
+  addCarStatus = false;
+  cars = ['Ford', 'Audi', 'BMW'];
+  items = [{id: 3, name: 'item 1'}, {id: 6, name: 'item 2'}, {id: 9, name: 'item 3'}]
+
+  constructor() {
+  }
+
+  addCar(){
+    this.addCarStatus = true;
+    this.cars.push(this.carName);
+    this.carName = '';
+  }
+}
