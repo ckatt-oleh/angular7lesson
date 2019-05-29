73773644a660805df3cd733238414797150ece21
Hordiienko Oleh <hordienko.oleh@gmail.com>
2019-05-29 17:14:29 +0300

    switch case using derectives

--- lessoncourse/src/app/app.component.ts
+++ lessoncourse/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   headerText = 'Создание новой дерективы';
+  items = [1,2,3,4,5];
+  current = 1;
+  onClick(number: number){
+    this.current = number;
+    console.log(this.current);
+  }
+
 }
