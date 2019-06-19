f6873f39d13f6b043317effc5c723a400a8c6383
Hordiienko Oleh <hordienko.oleh@gmail.com>
2019-06-19 12:18:09 +0300

    Get request

--- angularVsServerOrigin/src/app/app.component.ts
+++ angularVsServerOrigin/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CarService } from './cars.service';
 
 @Component({
@@ -6,15 +6,15 @@ import { CarService } from './cars.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
   constructor(private carsService: CarService){}
 
-  ngOnInit(): void {
+  loadCars() {
     this.carsService
       .getCars()
       .subscribe((response) => {
-        console.log(response)
+        console.log(response);
       });
   }
 
--- angularVsServerOrigin/src/app/cars.service.ts
+++ angularVsServerOrigin/src/app/cars.service.ts
@@ -7,6 +7,6 @@ export class CarService {
     constructor(private http: Http) {}
 
     getCars() {
-        return this.http.get('http://localhost:/3000/cars')
+        return this.http.get('http://localhost:3000/cars')
     }
 }
