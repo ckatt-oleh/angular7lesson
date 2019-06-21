f2bef6b8c3766e34805d48463d5345c67391daa5
Hordiienko Oleh <hordienko.oleh@gmail.com>
2019-06-21 15:26:22 +0300

    error handless

--- angularVsServerOrigin/src/app/cars.service.ts
+++ angularVsServerOrigin/src/app/cars.service.ts
@@ -1,15 +1,39 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable()
 
 export class CarService {
     constructor(private http: HttpClient) {}
 
-    getCars() {
-        return this.http.get('http://localhost:3000/cars')
-            .pipe(map(cars => { return cars}));
+    private handleError(error: HttpErrorResponse) {
+        if (error.error instanceof ErrorEvent) {
+          // A client-side or network error occurred. Handle it accordingly.
+          console.error('An error occurred:', error.error.message);
+        } else {
+          // The backend returned an unsuccessful response code.
+          // The response body may contain clues as to what went wrong,
+          console.error(
+            `Backend returned code ${error.status}, ` +
+            `body was: ${error.error}`);
+        }
+        // return an observable with a user-facing error message
+        return throwError(
+          'Something bad happened; please try again later.');
+      };
+
+    private headerOptions = {
+        headers: new HttpHeaders({
+            'Content-Type':  'application/json charset-utf8',
+            'Authorization': 'my-auth-token',
+           })
+    }
+
+    getCars() {        
+        return this.http.get('http://localhost:3000/cars', this.headerOptions)
+            .pipe(map(cars => { return cars}), catchError(this.handleError));
     }
 
     addCar(carName: string, carColor: string) {
